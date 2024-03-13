9616743b53389d10861b3f1261a11534a1490a0a
Moustafa-Abdelmaksoud <140589817+Moustafa-Abdelmaksoud@users.noreply.github.com>
2024-03-13 22:26:20 +0200

    Update add.php

--- admin/users/add.php
+++ admin/users/add.php
@@ -17,7 +17,7 @@
     
     if(!$error_fields){
         // Open the connection
-        $conn = mysqli_connect("localhost", "root", "J43_/brrI*8gJHPb", "shop_db");
+        $conn = mysqli_connect("localhost", "root", "", "shop_db");
         if (! $conn) {
             echo mysqli_connect_error();
             exit;
